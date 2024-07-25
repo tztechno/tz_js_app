7106471dd3a328edc67697ce451854766d431d7c
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-07-25 22:59:09 +0900

    Update main.js

--- 3_bingo/js/main.js
+++ 3_bingo/js/main.js
@@ -1,9 +1,15 @@
+
 function init() {
+    console.log("Init function called");
+    console.log("Board before initialization:", board);
     initializeBoard();
+    console.log("Board after initialization:", board);
     renderBoard();
+    console.log("Board rendering completed");
 
     const shuffleButton = document.getElementById('shuffleButton');
     shuffleButton.addEventListener('click', shuffleNumbers);
 }
 
+console.log("main.js loaded");
 init();
